e65eff9f071a9c0dbae702d2c7212d6f46f45d32
Joey Schoblaska <joey.schoblaska@gmail.com>
2013-10-16 11:43:33 -0500

    added move evaluation

--- Rakefile
+++ Rakefile
@@ -1,1 +1,5 @@
 require './lib/chess_blunders'
+
+task :console do
+  binding.pry
+end
--- lib/chess_blunders.rb
+++ lib/chess_blunders.rb
@@ -1,5 +1,26 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
 require './lib/passant'
 require './lib/ruby_uci'
 
 class ChessBlunders
+  def initialize(pgn_file)
+    @pgn = Passant::PGN::File.new(pgn_file)
+    @moves = @pgn.games.first.to_board.history
+    @uci = RubyUCI.new('stockfish', 12)
+  end
+
+  def evaluate_moves
+    @evaluations = []
+
+    @moves.each_with_index do |move, i|
+      @uci.moves = i == 0 ? '' : @moves[0..i-1].map{|m| m.to_uci}.join(' ')
+      evaluation = @uci.evaluate(move.to_uci)[:score][:num]
+      evaluation *= -1 unless i%2 == 0
+      @evaluations << evaluation
+    end
+  end
 end
